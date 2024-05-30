79bfbc388e8c5797f39aa87fe80ecc6b1c5304d7
zhanglei <zhanglei@fuzhi.ai>
2024-05-30 21:28:57 +0800

    update: 1.gen_patch_points和code_review的action改成分PatchFile（改动文件）进行分开review 2.code_review的action暂时注释掉confirm_commit的逻辑执行，待优化其效果后放开

--- examples/cr/code_review.py
+++ examples/cr/code_review.py
@@ -11,6 +11,8 @@ python3 cr/code_review.py
 """
 
 import asyncio
+import json
+from datetime import datetime
 
 from unidiff import PatchSet
 
@@ -19,19 +21,21 @@ from metagpt.ext.cr.roles.code_reviewer import CodeReviewer, PatchPoints
 from metagpt.schema import Message
 from metagpt.tools.libs.cr import PointManager
 
-INPUT_PATCH_FILE = EXAMPLE_DATA_PATH / "cr/mg_pr_1193_5483cc6-d0b00e3_small.patch"
-
+INPUT_PATCH_FILE = EXAMPLE_DATA_PATH / "cr/6.patch"
+patch_file_name = "6"
 
 async def main():
     # get points from existed result
     p = PointManager()
     points = await p.get_points()
 
-    patch: PatchSet = PatchSet(INPUT_PATCH_FILE.read_text())
+    patch: PatchSet = PatchSet(INPUT_PATCH_FILE.read_text(encoding='utf-8'))
 
     crer = CodeReviewer()
-    await crer.run(Message(content="code review on PR patch", instruct_content=PatchPoints(patch=patch, points=points)))
-
+    msg = await crer.run(Message(content="code review on PR patch", instruct_content=PatchPoints(patch=patch, points=points)))
+    with open(f'CR-{patch_file_name}-{datetime.timestamp(datetime.now())}-result.json', 'w', encoding='utf-8') as file:
+        file.writelines(json.dumps(msg.instruct_content.cr_comments, ensure_ascii=False))
+    return msg
 
 if __name__ == "__main__":
     asyncio.run(main())
